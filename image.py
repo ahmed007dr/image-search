b610ee55b5255ff4461f454d9aa03cdcc91264a4
ahmed007dr <ahmed007eg@gmail.com>
2024-05-16 04:13:40 +0300

    update code with pip install bing-image-downloader

--- image.py
+++ image.py
@@ -1,6 +1,6 @@
 import os
 import openpyxl
-from google_images_download import google_images_download
+from bing_image_downloader import downloader
 import time
 
 def create_output_directory(output_directory):
@@ -13,24 +13,17 @@ def load_excel_sheet(excel_file):
     return openpyxl.load_workbook(excel_file).active
 
 def search_and_download_images(sheet, output_directory):
-    # Initialize GoogleImagesDownload
-    response = google_images_download.googleimagesdownload()
-
     # Loop through each row in the first column of the Excel sheet
     for row in sheet.iter_rows(min_row=1, max_col=1, max_row=sheet.max_row, values_only=True):
         name = row[0]
         if name:
             print(f"Searching for images of '{name}'...")
-            # Search for images on Google Images
-            arguments = {"keywords": name, "limit": 1, "output_directory": output_directory}
-            try:
-                paths = response.download(arguments)
-                print(f"Downloaded image for '{name}'")
-            except Exception as e:
-                print(f"Error downloading image for '{name}': {e}")
+            # Search for images on Bing Images
+            downloader.download(name, limit=1, output_dir=output_directory, adult_filter_off=True, force_replace=False, timeout=60)
+            print(f"Downloaded image for '{name}'")
             
             # Introduce a delay between each image download request
-            time.sleep(3)  # Adjust the delay time as needed
+            time.sleep(1)  # Adjust the delay time as needed
 
 if __name__ == "__main__":
     # Specify the path to your Excel file
